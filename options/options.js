1b620ab55f740a44669c88015e06450ea099b98f
Crowdbotics <team@crowdbotics.com>
2023-12-29 15:49:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "interstatesengine-45224",
+  url: "https://interstatesengine-45224.botics.co",
+  api: "https://interstatesengine-45224.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
